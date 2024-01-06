486a5a295e81820cdb77bfcefc2bb5d820216f99
DarkDreamerr <155757250+DarkDreamerr@users.noreply.github.com>
2024-01-06 17:01:42 +0530

    Calculator

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var EnteredTextView: TextView
     lateinit var ResultTextView: TextView
 
-    var currentInput:String = ""
-    var currentOperator:String? = null
+    var currentInput: String = ""
+    var currentOperator: String? = null
     var firstOperand: Double = 0.0
     var isResultDisplayed: Boolean = false
 
@@ -24,94 +25,98 @@ class MainActivity : AppCompatActivity() {
 
         var buttons = arrayOf(
             R.id.One, R.id.Two, R.id.Three, R.id.Four, R.id.Five, R.id.Six, R.id.Seven, R.id.Eight,
-            R.id.Nine, R.id.ZeroZero, R.id.Zero, R.id.Equalto ,R.id.Decimal
+            R.id.Nine, R.id.ZeroZero, R.id.Zero, R.id.Equalto, R.id.Decimal
         )
         for (buttonID in buttons)
             findViewById<Button>(buttonID).setOnClickListener { onDigitClicked(it) }
     }
-}
 
-
-fun onDigitClicked(view: View){
-    if(isResultDisplayed){
-        ClearAll()
-        isResultDisplayed = false
-    }
-    currentInput += (view as Button).text.toString()
-    updateDisplay()
-}
-
-
-fun onOperatorClicked(operator: String){
-    if(currentInput.isNotEmpty()){
-        if(currentOperator != null){
-            calculateResult()
+    fun onDigitClicked(view: View) {
+        if (isResultDisplayed) {
+            ClearAll()
+            isResultDisplayed = false
         }
-        currentOperator = operator
-        firstOperand = currentInput.toDouble()
-        currentInput = ""
-        isResultDisplayed = false
-        updateDisplay()
-    }else if(isResultDisplayed){
-        currentOperator = operator
-        currentInput = firstOperand.toString()
-        isResultDisplayed = false
+        currentInput += (view as Button).text.toString()
         updateDisplay()
     }
-}
-fun ClearAll(){ //When Clicked on AC- All Clear
-    currentInput = ""
-    currentOperator = null
-    firstOperand = 0.0
-    isResultDisplayed = false
-    updateDisplay()
-}
-fun backspace(){
-    if(currentInput.isNotEmpty()){
-        currentInput = currentInput.substring(0,currentInput.length-1)
-        updateDisplay()
-    }
-}
 
-fun calculateResult() {
-    if(currentOperator != null && currentInput.isNotEmpty()){
-        val secondOperand = currentInput.toDouble()
-        when(currentOperator){
-            "+" ->firstOperand += secondOperand
-            "-" ->firstOperand -= secondOperand
-            "*" ->firstOperand *= secondOperand
-            "/" ->{
-                if(secondOperand != 0.0){
-                    firstOperand /= secondOperand
-                }else{
-                    Toast.makeText(this@MainActivity,"Division By Zero Error",Toast.LENGTH_SHORT).show()
-                    ClearAll()
-                    enteredTextView.text = "Error"
-                    return
-                }
+    fun onOperatorClicked(operator: String) {
+        if (currentInput.isNotEmpty()) {
+            if (currentOperator != null) {
+                calculateResult()
             }
-            "%" ->firstOperand = (firstOperand * secondOperand) / 100
+            currentOperator = operator
+            firstOperand = currentInput.toDouble()
+            currentInput = ""
+            isResultDisplayed = false
+            updateDisplay()
+        } else if (isResultDisplayed) {
+            currentOperator = operator
+            currentInput = firstOperand.toString()
+            isResultDisplayed = false
+            updateDisplay()
         }
+    }
+
+    fun ClearAll() { //When Clicked on AC- All Clear
+        currentInput = ""
         currentOperator = null
-        currentInput = firstOperand.toString()
-        isResultDisplayed = true
+        firstOperand = 0.0
+        isResultDisplayed = false
         updateDisplay()
-        ResultTextView.text = "=$firstOperand"
-
     }
-}
 
-fun updateDisplay() {
-    val expression = if(currentOperator != null){
-        "$firstOperand $currentOperator $currentInput"
-    }else{
-        currentInput
+    fun backspace() {
+        if (currentInput.isNotEmpty()) {
+            currentInput = currentInput.substring(0, currentInput.length - 1)
+            updateDisplay()
+        }
     }
-    enteredTextView.text = expression
-    ResultTextView.text = if(currentOperator != null){
-        "$firstOperand $currentOperator"
-    }else{
-        ""
+
+    fun calculateResult() {
+        if (currentOperator != null && currentInput.isNotEmpty()) {
+            val secondOperand = currentInput.toDouble()
+            when (currentOperator) {
+                "+" -> firstOperand += secondOperand
+                "-" -> firstOperand -= secondOperand
+                "*" -> firstOperand *= secondOperand
+                "/" -> {
+                    if (secondOperand != 0.0) {
+                        firstOperand /= secondOperand
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Division By Zero Error",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        ClearAll()
+                        EnteredTextView.text = "Error"
+                        return
+                    }
+                }
+
+                "%" -> firstOperand = (firstOperand * secondOperand) / 100
+            }
+            currentOperator = null
+            currentInput = firstOperand.toString()
+            isResultDisplayed = true
+            updateDisplay()
+            ResultTextView.text = "=$firstOperand"
+
+        }
+    }
+
+    fun updateDisplay() {
+        val expression = if (currentOperator != null) {
+            "$firstOperand $currentOperator $currentInput"
+        } else {
+            currentInput
+        }
+        EnteredTextView.text = expression
+        ResultTextView.text = if (currentOperator != null) {
+            "$firstOperand $currentOperator"
+        } else {
+            ""
         }
     }
 }
